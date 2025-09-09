c1748e731c3f08c6feb0eed793551267d48400c2
Nicholas obonyo abala <nicholasobonyo444@gmail.com>
2025-09-09 13:47:15 +0300

    Lab complete

--- server/app.py
+++ server/app.py
@@ -1,27 +1,26 @@
 # server/app.py
 #!/usr/bin/env python3
-
-from flask import Flask, make_response
+from flask import Flask, jsonify
 from flask_migrate import Migrate
-
 from models import db, Earthquake
 
 app = Flask(__name__)
+
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.json.compact = False
 
-migrate = Migrate(app, db)
 db.init_app(app)
-
+migrate = Migrate(app, db)
 
 @app.route('/')
 def index():
-    body = {'message': 'Flask SQLAlchemy Lab 1'}
-    return make_response(body, 200)
+    return jsonify({"message": "Flask SQLAlchemy Lab 1"}), 200
 
-# Add views here
-
+@app.route('/earthquakes')
+def get_earthquakes():
+    quakes = Earthquake.query.all()
+    return jsonify([quake.to_dict() for quake in quakes]), 200
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
+
--- server/models.py
+++ server/models.py
@@ -2,8 +2,23 @@ from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import MetaData
 from sqlalchemy_serializer import SerializerMixin
 
-metadata = MetaData()
+# Naming convention (helps with migrations)
+metadata = MetaData(
+    naming_convention={
+        "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+    }
+)
 
 db = SQLAlchemy(metadata=metadata)
 
-# Add models here
+class Earthquake(db.Model, SerializerMixin):
+    __tablename__ = 'earthquakes'
+
+    id = db.Column(db.Integer, primary_key=True)
+    magnitude = db.Column(db.Float, nullable=False)
+    location = db.Column(db.String, nullable=False)
+    year = db.Column(db.Integer, nullable=False)
+
+    def __repr__(self):
+        return f'<Earthquake {self.id} - Magnitude: {self.magnitude}>'
+
